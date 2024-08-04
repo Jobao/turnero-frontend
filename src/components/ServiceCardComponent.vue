f2390813eba02aed3f60d2c9e3d07bb84d999c5b
Joao Lucas Ramos Suarez <ramossuarezjl@gmail.com>
2024-08-03 21:59:08 -0300

    funciona el deshabilitar el boton de siguiente

--- src/components/ServiceCardComponent.vue
+++ src/components/ServiceCardComponent.vue
@@ -1,12 +1,14 @@
 <template >
-  <button class="border rounded mr-2">{{props.title}}</button>
+  <button class="border rounded mr-2 px-4 py-2" @click="props.onSelect(props.idServicio)">{{props.title}}</button>
 </template>
 
 <script setup lang="ts">
  
  const props = defineProps<{
   title:string,
-  descripcion:string
+  descripcion:string,
+  idServicio:number,
+  onSelect:Function
  }>()
 
 
--- src/views/ServiciosView.vue
+++ src/views/ServiciosView.vue
@@ -1,16 +1,27 @@
 <template>
   <div v-if="servicios" class="flex">
-    <ServiceCardComponent v-for="serv in servicios" :title="serv.titulo" :descripcion="serv.descripcion"></ServiceCardComponent>
+    <ServiceCardComponent v-for="serv in servicios" 
+    :title="serv.titulo" 
+    :descripcion="serv.descripcion" 
+    :idServicio="serv.idServicio" 
+    :onSelect="seleccionServicio"
+    :key="serv.idServicio">
+
+    </ServiceCardComponent>
+
+    <button class=" px-4 py-2 rounded-full" :class="siguienteButtonClass">Siguiente</button>
   </div>
 </template>
 
 <script setup lang="ts">
 import { fakeDataProfesional } from '@/assets/fakeData'
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 import { useRoute } from 'vue-router'
 import ServiceCardComponent from '@/components/ServiceCardComponent.vue'
 
 const route = useRoute()
+const seleccionado = ref(-1)
+
 const servicios = ref(getServicios(Number(route.params.profesionalID)))
 function getServicios(profesionalID: number) {
   let profesional = fakeDataProfesional.find((prof) => {
@@ -19,4 +30,27 @@ function getServicios(profesionalID: number) {
 
   return profesional?.servicios
 }
+
+function seleccionServicio(servicioID: number) {
+  if (servicioID === seleccionado.value) {
+    seleccionado.value = -1
+  } else {
+    seleccionado.value = servicioID
+  }
+  console.log(seleccionado.value);
+}
+
+const siguienteButtonClass = computed({
+  get:()=>{
+    if(seleccionado.value >= 0){
+      return 'bg-blue-600 '
+    }
+    return ' pointer-events-none bg-gray-400'
+    
+  },
+  set:(val)=>{}
+  
+})
+
+
 </script>
