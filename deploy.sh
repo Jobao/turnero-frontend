a3e4e5aa532509f95f01545cbf8ea0fa195766dc
Joao Lucas Ramos Suarez <ramossuarezjl@gmail.com>
2024-08-06 11:03:45 -0300

    .

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,19 @@
 set -e
 
 # build
-git subtree add --prefix dist git@github.com:Jobao/turnero-frontend.git gh-pages
+#git subtree add --prefix dist git@github.com:Jobao/turnero-frontend.git gh-pages
 npm run build
 
 #git subtree push --prefix dist origin gh-pages
 # navigate into the build output directory
-#cd dist
+cd dist
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
-#git init
-#git add -A
-#git commit -m 'deploy'
+git init
+git add -A
+git commit -m 'deploy'
 
-#git push -f git@github.com:Jobao/turnero-frontend.git main:gh-pages
+git push -f git@github.com:Jobao/turnero-frontend.git main:gh-pages
 #cd -
